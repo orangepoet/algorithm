0c94fb737c3aa301518244b306b05388602eed74
orange.cheng <orange.cheng@nio.com>
2023-08-03 12:28:49 +0800

    gameTest

--- src/main/java/cn/orangepoet/inaction/algorithm/Game.java
+++ src/main/java/cn/orangepoet/inaction/algorithm/Game.java
@@ -43,25 +43,6 @@ public class Game {
         node.val = bestValue;
         return bestValue;
     }
-
-    public static void main(String[] args) {
-        var terminals = new int[] { 15, 58, 81, 74, 88, 15, 20, 92, 9, 62, 82, 92, 54, 17, 63, 60 };
-        Node[] nodes = new Node[terminals.length];
-        for (int i = 0; i < terminals.length; i++) {
-            nodes[i] = new Node(terminals[i]);
-        }
-        while (nodes.length > 1) {
-            Node[] parent = new Node[nodes.length / 2];
-            for (int i = 0; i < parent.length; i++) {
-                parent[i] = new Node(nodes[i * 2], nodes[i * 2 + 1]);
-            }
-            nodes = parent;
-        }
-
-        int bestValue = new Game().alphaBetaNegMax(nodes[0], Integer.MIN_VALUE, Integer.MAX_VALUE);
-        // int bestValue = new Game().negMax(nodes[0]);
-        System.out.println(bestValue);
-    }
 }
 
 class Node {
---
+++ src/test/groovy/cn/orangepoet/inaction/algorithm/GameTest.groovy
@@ -0,0 +1,46 @@
+package cn.orangepoet.inaction.algorithm
+
+import spock.lang.Specification
+
+class GameTest extends Specification {
+    
+    def game = new Game()
+
+    def makeTree() {
+        var terminals = [ 15, 58, 81, 74, 88, 15, 20, 92, 9, 62, 82, 92, 54, 17, 63, 60 ] as int[]
+        Node[] nodes = new Node[terminals.length]
+        for (int i = 0; i < terminals.length; i++) {
+            nodes[i] = new Node(terminals[i])
+        }
+        while (nodes.length > 1) {
+            Node[] parent = new Node[nodes.length / 2];
+            for (int i = 0; i < parent.length; i++) {
+                parent[i] = new Node(nodes[i * 2], nodes[i * 2 + 1]);
+            }
+            nodes = parent
+        }
+        return nodes[0]
+    }
+
+    def '最大最小搜索'() {
+        given:
+        Node node = makeTree()
+
+        when:
+        def bv = game.negMax(node)
+
+        then:
+        bv == 60
+    }
+
+    def '最大最小AlphBeta剪枝搜索'() {
+        given:
+        Node node = makeTree()
+
+        when:
+        def bv = game.alphaBetaNegMax(node, Integer.MIN_VALUE, Integer.MAX_VALUE)
+
+        then:
+        bv == 60
+    }
+}
